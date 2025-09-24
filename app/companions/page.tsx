be3fa19cdd42ffdbdfe4bf26e3756069b2bb1f97
irfan-fauzi <=>
2025-09-24 17:05:19 +0700

    minor

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,31 +1,31 @@
-import CompanionCard from "@/components/CompanionCard"
-import { getAllCompanion } from "@/lib/actions/companion.action"
-import { getSubjectColor } from "@/lib/utils"
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanion } from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 
+const CompanionPage = async ({ searchParams }: SearchParams) => {
+  const filter = await searchParams;
+  const subject = filter.subject ? filter.subject : "";
+  const topic = filter.topic ? filter.topic : "";
 
-const CompanionPage = async({searchParams} : SearchParams ) => {
-  const filter = await searchParams
-  const subject = filter.subject ? filter.subject : ''
-  const topic = filter.topic ? filter.topic : ''
-
-  const companion = await getAllCompanion({subject, topic})
-  
+  const companion = await getAllCompanion({ subject, topic });
 
   return (
     <main>
-      <section className="flex justify-between gap-4 max-sm:flex-col">
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
         <h1>Companion Library</h1>
-        <div className="flex gap-4">Filters</div>
+        <div className='flex gap-4'>Filters</div>
       </section>
-      <section className="companions-grid">
-        {
-          companion.map((companion) => (
-            <CompanionCard key={companion.id} color={getSubjectColor(companion.subject)} {...companion} />
-          ))
-        }
+      <section className='companions-grid'>
+        {companion.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            color={getSubjectColor(companion.subject)}
+            {...companion}
+          />
+        ))}
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default CompanionPage
+export default CompanionPage;
