28eacabca35fc68b6d32a8011a143581c19f6fa2
irfan-fauzi <=>
2025-09-25 10:47:28 +0700

    ui session detail companion

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getCompanion } from "@/lib/actions/companion.action";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 interface CompanionSessionPageProps {
@@ -10,6 +11,7 @@ interface CompanionSessionPageProps {
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
   const detailCompanion = await getCompanion(id);
+  const {name, subject, title, topic, duration} = detailCompanion
   const user = await currentUser();
 
   if (!user) redirect("/sign-in");
@@ -22,9 +24,28 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           <div
             className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden'
             style={{
-              backgroundColor: getSubjectColor(detailCompanion.subject),
+              backgroundColor: getSubjectColor(subject),
             }}
-          ></div>
+          >
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt='subject'
+              width={35}
+              height={35}
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <div className='flex items-center gap-2'>
+              <p className='font-bold text-2xl'>{name}</p>
+              <div className='subject-badge max-sm:hidden'>
+                {subject}
+              </div>
+            </div>
+            <p className="text-lg">{topic}</p>
+          </div>
+        </div>
+        <div className="items-start text-2xl max-md:hidden">
+          {duration} minutes
         </div>
       </article>
     </main>
